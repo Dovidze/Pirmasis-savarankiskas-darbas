881aa75c71cc03e9a0d75a3dbe1deb2e70e436a7
Dovidze <114766506+Dovidze@users.noreply.github.com>
2022-10-28 19:32:28 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int main(){
+  
+  string Pavarde, Vardas, ND1, ND2, ND3, ND4, ND5;
+  vector<string>PV;
+  vector<double>NDp;
+  vector<double>Gp;
+  
+  //txt dokumentas
+  string file;
+  file = 'Studentai.txt';
+  
+  //eiluciu skaicius
+  int i = 0;
+  
+  ifstream coedd(file);
+  if (coeff.is_open())
+  {
+    string line;
+    getline(coeff, line);
+    
+    while(!coeff.eof())
+    {
+      getline(coeff, Pavarde)
+    }
+    
+  }
+
+}
