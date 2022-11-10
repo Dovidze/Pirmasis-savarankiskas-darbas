d758977e512741f6342a45295d5eb1c6aa55b16e
Dovidze <114766506+Dovidze@users.noreply.github.com>
2022-11-10 16:12:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,34 @@
 #include <string>
 #include <iomanip>
 #include <sstream>
-#include <vector>
-#include <fstream
+#include <fstream>
 
+using std::endl;
+using std::string;
 using std::cout;
 using std::cin;
 
-int main(){
-  string vardas, pavarde; 
-  cout << "Iveskite studento Varda" << endl;
-  cin >> vardas;
-  cout << "Iveskite studento Pavarde" << endl;
-  cin >> pavarde;
+int main() {
+	string vardas, pavarde;
+	double galutinis = 0, vid = 0;
+	int egzpaz, paz, pazsk;
+
+	cout << "Iveskite studento Varda" << endl;
+	cin >> vardas;
+	cout << "Iveskite studento Pavarde" << endl;
+	cin >> pavarde;
+	cout << "Iveskite studento pazymiu skaiciu" << endl;
+	cin >> pazsk;
+	cout << "Iveskite studento pazymius" << endl;
+	for (int i = 0; i < pazsk; i++) 
+	{
+		cin >> paz;
+		vid = vid + paz;
+	}
+	vid = (vid / pazsk) * 0.4;
+	cout << "Iveskite egzamino pazymi" << endl;
+	cin >> egzpaz;
+	galutinis = vid + 0.6 * egzpaz;
+	cout << "Studento " << vardas << " " << pavarde << "galutinis pazymis yra " << galutinis;
 
 }
